46fdabc9d878677efbbac72f78f99de074656daf
jochengaiser.de <jochengaiser.de@gmail.com>
2013-01-09 13:49:53 +0000

    user script v 0.1 not working yet

---
+++ userscript/replace_video.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @match http://www.google.com/*
+// @name            HTML5 video replace
+// @namespace       media-berry
+// @description     Replaces HTML5 to local link to start native player
+// @include         *
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @version         0.1
+// ==/UserScript==
+alert("juhu");
+$(document).ready(function () {
+
+});
